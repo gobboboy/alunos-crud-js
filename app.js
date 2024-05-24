f219b3eeb23a555d59f6825f6ba3f59440e2dc8b
Marcos Gobbo <marcoszgobbo@gmail.com>
2024-05-24 14:01:32 -0300

    Generating JWT for users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import './src/database';
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokenRoutes from './src/routes/tokenRoutes';
 
 class App {
   constructor() {
@@ -20,8 +21,9 @@ class App {
   }
 
   routes() {
-    this.app.use('/', homeRoutes)
-    this.app.use('/users/', userRoutes)
+    this.app.use('/', homeRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 
