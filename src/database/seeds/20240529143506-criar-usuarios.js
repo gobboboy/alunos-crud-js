b0176aa30d06977934989b6b315afd7bbe20afdd
Marcos Gobbo <marcoszgobbo@gmail.com>
2024-05-29 11:49:21 -0300

    Creating users seeds and populating db with sequelize

---
+++ src/database/seeds/20240529143506-criar-usuarios.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+      {
+      nome: 'Luiz',
+      email: 'luiz@gmail.com',
+      password_hash: await bcrypt.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date()
+      },
+      {
+      nome: 'john',
+      email: 'john@gmail.com',
+      password_hash: await bcrypt.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date()
+      },
+      {
+      nome: 'Joana',
+      email: 'joana@gmail.com',
+      password_hash: await bcrypt.hash('123456', 8),
+      created_at: new Date(),
+      updated_at: new Date()
+      },
+    ],
+    {}
+  );
+
+  },
+
+  async down () {
+  }
+};
