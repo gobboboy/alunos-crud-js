5a9da7659f3c2841877ff1179196457c608f6b45
Marcos Gobbo <marcoszgobbo@gmail.com>
2024-06-03 10:42:50 -0300

    Setting up multer to receive Aluno images

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ dotenv.config();
 import './src/database';
 
 import express from 'express';
+
+import fotoRoutes from './src/routes/fotoRoutes';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
@@ -25,7 +27,8 @@ class App {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
     this.app.use('/tokens/', tokenRoutes);
-    this.app.use('/alunos', alunoRoutes);
+    this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
   }
 }
 
---
+++ src/controllers/FotoController.js
@@ -0,0 +1,7 @@
+class FotoController {
+  async store(req, res) {
+    return res.json('Index');
+  }
+}
+
+export default new FotoController();
